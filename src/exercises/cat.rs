e21e0612530181493188c93fc7fce093ea8f48f1
Admin <landrzejewski.poczta@gmail.com>
2025-07-31 12:35:46 +0200

    day 2

---
+++ src/exercises/cat.rs
@@ -0,0 +1,7 @@
+fn show_help() {
+    println!("Usage:");
+    println!("cat [option] file1 file2 ...");
+    println!("options:");
+    println!("  -n - show line numbers");
+    println!("  -nb - show line numbers, ignore blank lines");
+}
--- src/exercises/mod.rs
+++ src/exercises/mod.rs
@@ -5,3 +5,5 @@ pub mod guess_game;
 pub mod tic_tac_toe;
 pub mod basic_io;
 pub mod budget;
+pub mod echo;
+mod cat;
